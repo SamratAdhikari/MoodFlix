86217e70ce92497407440450cd9af30386265c03
ubermensch <samratmetaladhikari@gmail.com>
2024-10-15 10:11:12 +0545

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ from movie import movie_recommendation
 
 # Sidebar with custom subtitle and options
 with st.sidebar:
-    st.title('Welcome to MoodFlix')
+    st.title('Welcome to TheMoodFlix')
     st.subheader('Pick your poison')
     selected = option_menu(
         "",                 
-        ["BookFlix", "MovieFlix"],             
+        ["Books", "Movies"],             
         icons=['book', 'film'],              
         menu_icon="cast",                    
         default_index=0                      
     )
 
 # Show BookPal or MoviePal page based on selection
-if selected == "BookFlix":
+if selected == "Books":
     book_recommendation()
 else:
     movie_recommendation()
